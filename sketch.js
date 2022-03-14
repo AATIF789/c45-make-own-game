b4ea6a19f58e841a12e8e794149f75071d1271b4
AATIF789 <86237190+AATIF789@users.noreply.github.com>
2022-03-14 18:16:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ var ground, backgroundImg,invisibleGround
 var enemy,enemyAttack,enemyRun,enemyImg
 var girl,girlImg ,girlRunning, girlColliding
 var score
-var obstacle
+
 var gameOver,gameOverImg, restart, restartImg
 var obstaclesGroup, obstacle1
 
@@ -20,21 +20,20 @@ function preload(){
   girlImg=loadImage("girl image.png");
   restartImg=loadImage("restart.png");
   gameOverImg=loadImage("game over.png");
-  girlColliding=loadImage("girl collided.png");
+  girlColliding=loadAnimation("girl collided.png");
   obstacle1=loadImage("obstacle 1.png");
 }
 function setup() {
   createCanvas(600,500);
-  obstacle= createSprite(660,450,10,40)
   ground=createSprite(0,0,0,0)
   ground.addImage("backgroundImg",backgroundImg)  
   ground.scale=1.4
   ground.velocityX=-1
 
-  girl=createSprite(520,410,20,10)
-  girl.addImage("girlImg",girlImg)
+  girl=createSprite(300,410,20,10)
+  //girl.addImage("girlImg",girlImg)
   girl.addAnimation("girlRunning",girlRunning)
-  girl.addImage("girlColliding",girlColliding)
+  girl.addAnimation("girlColliding",girlColliding)
   girl.scale=0.2
 
   enemy = createSprite(50, 410, 600, 10);
@@ -49,7 +48,7 @@ function setup() {
   gameOver=createSprite(300,100)
   gameOver.addImage(gameOverImg)
 
-  restart=createSprite(350,115)
+  restart=createSprite(350,160)
   restart.addImage(restartImg)
 
   obstaclesGroup = new Group();
@@ -61,7 +60,8 @@ function setup() {
 
 function draw() 
 {
-  girl.velocityY = girl.velocityY + 0.8;
+  background("black");
+  girl.velocityY = girl.velocityY + 0.7;
   girl.collide(invisibleGround);
 
   enemy.velocityY=girl.velocityY+0.8
@@ -69,70 +69,78 @@ function draw()
 
 
   if(gameState===PLAY){
-    spawnObstacles();
 
     gameOver.visible=false
     restart.visible=false
     score = score + Math.round(getFrameRate() / 60);
+    spawnObstacles();
 
     if (obstaclesGroup.isTouching(enemy)) {
       enemy.velocityY = -12;
     }
-    ground.velocityX = -(4 + 3 * score / 100);
 
+    ground.velocityX = -(4 + 3 * score / 100);
+    
     if (ground.x < 0) {
       ground.x = ground.width / 2;
     }
-    if ((keyDown("space") && girl.y >= 220)) {
+    //console.log(girl.y)
+    if ((keyDown("space") && girl.y >= 419.59)) {
       girl.velocityY = -12;
+    }
+    if (obstaclesGroup.isTouching(girl)) {
+      gameState = END;
+    } 
   }
-  if (girl.isTouching(obstaclesGroup)) {
-    gameState = END;
-  }  
-
   else if (gameState === END) {
     gameOver.visible = true;
     restart.visible = true;
     ground.velocityX = 0;
     girl.velocityY = 0
-    girl.changeImage("girlImage", girlImage);
+    girl.changeAnimation("girlColliding", girlColliding);
     enemy.changeAnimation("enemyAttack", enemyAttack);
     enemy.x = girl.x;
-    if (enemy.isTouching(girl)) {
-      girl.changeImage("girl_collided", girl_collided);
-      enemy.changeImage("enemyImg", enemyImg);
-    }
     //set lifetime of the game objects so that they are never destroyed
     obstaclesGroup.setLifetimeEach(-1);
     obstaclesGroup.setVelocityXEach(0);
 
     if (mousePressedOver(restart)) {
       reset();
-    }
+    } 
 
-  background("black");
-  drawSprites()
-}
+
   }
+  
+  
+  drawSprites()
+  fill("blue");
+  textSize(15);
+  text("Score: " + score, 450, 50);
 }
 function reset() {
   gameState = PLAY;
   gameOver.visible = false;
   restart.visible = false;
-  girl.changeAnimation("girl_running", girl_running);
+  girl.changeAnimation("girlRunning", girlRunning);
   obstaclesGroup.destroyEach();
   score = 0;
   enemy.x = 50;
 }
 
 function spawnObstacles(){
-  if(frameCount %60===0){
-   
-    obstacle.velocityX=-6
-    obstacle.addImage(obstacle1)
+  if (frameCount % 100 === 0) {
+    var obstacle = createSprite(600, 450, 10, 40);
+
+
+    obstacle.velocityX = -(6+ score/100)
+
+    //generate random obstacles
+    var rand = Math.round(random(1, 6));
+    obstacle.addImage(obstacle1);
     obstacle.scale = 0.1;
     obstaclesGroup.add(obstacle);
     obstacle.debug = false;
     obstacle.setCollider("circle", 0, 0, 1);
+  
   }
 }
